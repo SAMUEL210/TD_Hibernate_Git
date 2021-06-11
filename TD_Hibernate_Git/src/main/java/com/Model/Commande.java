bcb1211364a4434754766e6278ad0d02f60a3273
Samuel <samuel210@live.fr>
2021-06-11 11:40:13 +0200

    Commande et CommandeServices commencé mais pas finis encore

--- TD_Hibernate_Git/src/main/java/com/Model/Commande.java
+++ TD_Hibernate_Git/src/main/java/com/Model/Commande.java
@@ -9,6 +9,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
@@ -21,21 +22,17 @@ public class Commande {
 	@Column(name = "id", nullable = false, unique = true)
 	private int id;
 	
-	@OneToMany(cascade=CascadeType.ALL)
+	@ManyToOne(cascade=CascadeType.ALL)
 	@JoinColumn(name="client_id")
-	private int client_id;
+	private int client;
 	
-	@OneToMany(cascade=CascadeType.ALL)
-	@JoinColumn(name="client_id")
-	private int produit_id;
+	@ManyToOne(cascade=CascadeType.ALL)
+	@JoinColumn(name="produit_id")
+	private int produit;
 	
 	@Column(name = "date", nullable = true, unique = false, length = 50)
 	private String date;
 	
-	@OneToMany(cascade=CascadeType.ALL)
-	@JoinColumn(name="client_id")
-	private Set<Produit> produits;
-
 
 	public Commande() {
 		super();
@@ -43,9 +40,42 @@ public class Commande {
 
 	public Commande(int client_id, int produit_id, String date) {
 		super();
-		this.client_id = client_id;
-		this.produit_id = produit_id;
+		this.client = client_id;
+		this.produit = produit_id;
 		this.date = date;
 	}
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getClient() {
+		return client;
+	}
+
+	public void setClient(int client) {
+		this.client = client;
+	}
+
+	public int getProduit() {
+		return produit;
+	}
+
+	public void setProduit(int produit) {
+		this.produit = produit;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	
 
 }
